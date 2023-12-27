37e95c729cfd3133e7771e1b18c15f959eb4ea92
Raszero <travisjcelli95@gmail.com>
2023-12-27 15:47:38 -0600

    created get.findAll to push to homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,30 @@
 const router = require("express").Router();
 const { User, Blog } = require("../models");
 
+router.get('/', async (req, res) => {
+    try {
+        // get all blogs and associated user name 
+        const blogData = await Blog.findAll(
+            {
+            include: [
+                {
+                model: User,
+                attributes: ['name']
+            }
+        ]
+        })
 
+        // serialize data
+        const blogs = blogData.map((blog) => blog.get({plain: true}))
+        res.status(200).json(blogs)
+        // res.render(('homepage', {
+        //     projects,
+        //     logged_in: req.session.logged_in
+        // }
+        // ))
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
 module.exports = router;
