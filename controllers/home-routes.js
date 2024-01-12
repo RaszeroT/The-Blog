b3aaaf35788e0f53619e97ce5a4b3f96318bbca6
Raszero <travisjcelli95@gmail.com>
2024-01-12 15:56:30 -0600

    created dashboard handlebar and homeroute

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { User, Blog, Comment } = require("../models");
+const { findAndCountAll } = require("../models/User");
 const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
@@ -34,30 +35,46 @@ router.get("/blog/:id", withAuth, async (req, res) => {
   try {
     const blogId = await Blog.findByPk(req.params.id, {
       include: [
-        { model: User, 
-          attributes: ["username"] },
-          {
-            model: Comment,
-            attributes: ["comment"],
-            include: [
-              {
-                model: User,
-                attributes: ["username"],
-              },
-            ],
-          },
+        { model: User, attributes: ["username"] },
+        {
+          model: Comment,
+          attributes: ["comment"],
+          include: [
+            {
+              model: User,
+              attributes: ["username"],
+            },
+          ],
+        },
       ],
     });
     //   return simplified data
     const blog = blogId.get({ plain: true });
     console.log(blog);
-    res.render('blogPage', {
-        ...blog,
-        logged_in: req.session.logged_in
-    }
-    )
+    res.render("blogPage", {
+      ...blog,
+      logged_in: req.session.logged_in,
+    });
   } catch (error) {
     res.status(500).json(error);
+  }
+});
+
+router.get("/dashboard", withAuth, async (req, res) => {
+  try {
+    const blogData = await Blog.findAll({
+      where: { user_id: req.session.user_id },
+      include: [{ model: User, attributes: ["username"] }],
+    });
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+    console.log(blogs);
+    res.render("dashboard", {
+      blogs,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
@@ -69,17 +86,16 @@ router.get("/signup", (req, res) => {
   res.render("signup");
 });
 
-router.get("/login", (req,res) => {
+router.get("/login", (req, res) => {
   if (req.session.logged_in) {
-    res.redirect("/")
-    return
+    res.redirect("/");
+    return;
   }
-  res.render("login")
-})
+  res.render("login");
+});
 
-router.get('/logout', (req,res) => {
-    res.redirect("/")
-})
+router.get("/logout", (req, res) => {
+  res.redirect("/");
+});
 
 module.exports = router;
-
